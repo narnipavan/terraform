ad74d9f34025fa56e11920cdeb59870a4afa80fd
Pavan Narni <mail.pavan08@gmail.com>
2021-03-23 23:15:51 -0500

    added security group

---
+++ prod.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "prod_files"{
+  bucket = "jamjamtfiles20191118"
+  acl    = "private"
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web"{
+  name        = "prod_web"
+  description = "Allow standard http and https port inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  
+
+   }
+
+  tags = {
+
+  "Terraform" : "true"
+   }
+}
